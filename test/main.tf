1aa6f50ef57ee97e8f53f3e4d1b750760d2b7a96
Robert Hagen <rdh@stealthllama.org>
2019-06-28 01:02:00 -0400

    Added test directory

---
+++ test/main.tf
@@ -0,0 +1,42 @@
+############################################################################################
+# Copyright 2019 Palo Alto Networks.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#   http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+############################################################################################
+
+
+provider "google" {
+  version = "~> 2.8"
+  credentials = "${var.gcp_credential}"
+  project     = "${var.gcp_project_id}"
+  region      = "${var.gcp_region}"
+  zone        = "${var.gcp_zone}"
+}
+
+resource "google_compute_instance" "vm_instance" {
+  name         = "terraform-instance"
+  machine_type = "f1-micro"
+
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-9"
+    }
+  }
+
+  network_interface {
+    # A default network is created for all GCP projects
+    network       = "default"
+    access_config {
+    }
+  }
+}
